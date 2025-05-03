c3faf0260baa4e277e3a1e6df7d449dbeefb488b
Belgaied2b <belgaied2b@gmail.com>
2025-05-03 04:41:05 +0200

    Update kucoin_utils.py

--- kucoin_utils.py
+++ kucoin_utils.py
@@ -1,11 +1,12 @@
-from kucoin.futures import Market
+from kucoin.client import Market
 import pandas as pd
 import time
 
-client = Market()
+# Connexion à l'API KuCoin Futures
+client = Market(url='https://api-futures.kucoin.com')
 
 def get_kucoin_perps():
-    markets = client.get_contract_symbols()
+    markets = client.get_symbols()
     return [m['symbol'] for m in markets if m['quoteCurrency'] == 'USDT']
 
 def fetch_klines(symbol, interval="4hour", limit=150):
