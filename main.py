f8c62d4c886bcef0bbf013d89c3192e1cbc06f26
Belgaied2b <belgaied2b@gmail.com>
2025-10-19 22:38:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys, pathlib
+ROOT = pathlib.Path(__file__).resolve().parent
+if str(ROOT) not in sys.path: sys.path.insert(0, str(ROOT))
 import os, asyncio, logging, contextlib
 from typing import Dict, Any, List
 
